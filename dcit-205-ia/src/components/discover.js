0785c858e99b6190d4e9663027bef51169a05ed7
Kwabena Owusu-Darko <kobbyowusudarko@gmail.com>
2023-11-23 21:09:18 +0000

    Discover component

---
+++ dcit-205-ia/src/components/discover.js
@@ -0,0 +1,13 @@
+
+
+export default function Discovery(){
+    return (
+        <>
+            <section className="discovery">
+                <h2>DISCOVER YOUR FUTURE IN COMPUTER SCIENCE</h2>
+                <p>Explore our cutting-edge programs and join Ghana's future tech force.</p>
+                <button className="secondary-button" style={{fontSize: '1.3rem', margin:'auto'}}>Apply Now</button>
+            </section>
+        </>
+    )
+}
