792324eb956312e443eb9321cb694edcb0662041
Kwabena Owusu-Darko <kobbyowusudarko@gmail.com>
2023-11-23 21:08:46 +0000

    "custom data"

---
+++ dcit-205-ia/src/data.js
@@ -0,0 +1,101 @@
+
+export const courses = [
+    {
+        title: 'Undergraduates',
+        periodToGrad: '4-Year',
+        duration: '2023 - 2027',
+        description: 'Open the door to sought-after technology careers with a world-class Bachelor of Science in Computer Science degree.',
+        img: '/img/undergraduate.jpg'
+    },
+    {
+        title: 'Graduates',
+        periodToGrad: '1-Year & 2-Year',
+        duration: '2023 - 2024/2025',
+        description: 'Embrace world class graduate education with unique blend of cutting-edge research and industry experience.',
+        img: '/img/graduate.jpg'
+    },
+    {
+        title: 'Postgraduates',
+        periodToGrad: '4-Year',
+        duration: '2023 - 2027',
+        description: 'Our PHDs lead the effort to extend the boundaries of knowledge through quality and industry relevant research.',
+        img: '/img/postgraduate.jpg'
+    },
+    {
+        title: 'Short Courses',
+        periodToGrad: '8 Weeks',
+        duration: '2023',
+        description: 'Our eight week intensive certificate course is open to senior high school graduates, tertiary students, school graduates, workers and the general public.',
+        img: '/img/short.jpg'
+    }
+];
+
+
+export const faculty = [
+    {
+        name: 'Ebenezer Owusu',
+        title: 'Head of Department',
+        img: '/img/eo.jpeg',
+        description: 'From Linkedin',
+        socials: {
+            linkedin: 'https://www.linkedin.com/in/ebenezer-owusu-883b3634/',
+            twitter: '',
+            facebook: ''
+        }  
+    },
+    {
+        name: 'Michael Agbo Tettey Soli',
+        title: 'Lecturer',
+        img: 'https://www.polymorphlabs.io/static/media/Soli.1ce44e8e40e1a2140c76.webp',
+        description: 'From LinkedIn',
+        socials: {
+            linkedin: '',
+            twitter: '',
+            facebook: ''
+        }  
+    },
+    {
+        name: 'Mark Atta Mensah',
+        title: 'Lecturer',
+        img: 'https://yt3.googleusercontent.com/ayErlUnVQjMrqvyQ6l-T3Y3rl45WgVjZVLECbUP5iH44KhE1T-pVa20STjd5El6qmwhjIXEHFWY=s176-c-k-c0x00ffffff-no-rj',
+        description: 'From LinkedIn',
+        socials: {
+            linkedin: 'https://www.linkedin.com/in/markmensah/?originalSubdomain=gh',
+            twitter: '',
+            facebook: ''
+        }  
+    },
+    {
+        name: 'Paul Nii Tackie Ammah',
+        title: 'Lecturer',
+        img: 'https://www.polymorphlabs.io/static/media/paul.830a8db989ed5598c07d.jpg',
+        description: 'From LinkedIn',
+        socials: {
+            linkedin: '',
+            twitter: '',
+            facebook: ''
+        }  
+    },
+    {
+        name: 'Christian Abrokwa',
+        title: 'Teaching Assistant',
+        img: '/img/ca.jpeg',
+        description: 'From Linkedin',
+        socials: {
+            linkedin: 'https://www.linkedin.com/in/daquiver/?originalSubdomain=gh',
+            twitter: '',
+            facebook: ''
+        }  
+    },
+    {
+        name: 'Phillipa Bennett-Eghan',
+        title: 'Teaching Assistant',
+        img: '/img/pbe.jpeg',
+        description: 'From LinkedIn',
+        socials: {
+            linkedin: 'https://www.linkedin.com/in/phillipa-bennett-eghan/?originalSubdomain=gh',
+            twitter: '',
+            facebook: ''
+        }  
+    },
+]
