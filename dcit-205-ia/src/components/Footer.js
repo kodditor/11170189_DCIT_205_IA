d848b76946d16d4baf29194f94c00f08dbafc6fb
Kwabena Owusu-Darko <kobbyowusudarko@gmail.com>
2023-11-24 21:12:27 +0000

    Footer with links updated

--- dcit-205-ia/src/components/Footer.js
+++ dcit-205-ia/src/components/Footer.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 
 
 export default function Footer() {
@@ -10,14 +11,14 @@ export default function Footer() {
             <footer>
                 <section>
                     <div className="footer-body">
-                        <img src="/img/logo.png"/>
+                    <Link to="/"><img src="/img/logo.png" alt="UG Department of Computer Science Logo"/></Link>
                         <ul>
-                            <li>PROGRAMS</li>
-                            <li>FACULTY</li>
-                            <li>CONTACT US</li>
-                            <li>ABOUT US</li>
-                            <li>STUDENT PROJECTS</li>
-                            <li>EVENTS</li>
+                            <Link to="/programs"><li>PROGRAMS</li></Link>
+                            <Link to='/faculty'><li>FACULTY</li></Link>
+                            <Link to='/contact'><li>CONTACT US</li></Link>
+                            <Link to='/about'><li>ABOUT US</li></Link>
+                            <Link to="/projects"><li>STUDENT PROJECTS</li></Link>
+                            <Link to='/events'><li>EVENTS</li></Link>
                         </ul>
                     </div>
                     <div className="copy">Copyright &copy; {year} Department of Computer Science, University of Ghana</div>
