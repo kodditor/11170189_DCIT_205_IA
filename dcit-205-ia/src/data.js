c388ca282fc51c7566742b71b426c04f30c05a69
Kwabena Owusu-Darko <kobbyowusudarko@gmail.com>
2023-11-24 21:10:58 +0000

    Fine-tuned our "data"

--- dcit-205-ia/src/data.js
+++ dcit-205-ia/src/data.js
@@ -1,6 +1,7 @@
 
 export const courses = [
     {
+        id: "UG",
         title: 'Undergraduates',
         periodToGrad: '4-Year',
         duration: '2023 - 2027',
@@ -8,6 +9,7 @@ export const courses = [
         img: '/img/undergraduate.jpg'
     },
     {
+        id: "GR",
         title: 'Graduates',
         periodToGrad: '1-Year & 2-Year',
         duration: '2023 - 2024/2025',
@@ -15,6 +17,7 @@ export const courses = [
         img: '/img/graduate.jpg'
     },
     {
+        id: "PG",
         title: 'Postgraduates',
         periodToGrad: '4-Year',
         duration: '2023 - 2027',
@@ -22,6 +25,7 @@ export const courses = [
         img: '/img/postgraduate.jpg'
     },
     {
+        id: "SC",
         title: 'Short Courses',
         periodToGrad: '8 Weeks',
         duration: '2023',
@@ -33,10 +37,11 @@ export const courses = [
 
 export const faculty = [
     {
+        id:'EO',
         name: 'Ebenezer Owusu',
         title: 'Head of Department',
         img: '/img/eo.jpeg',
-        description: 'From Linkedin',
+        description: 'Specialties: Pattern Recognition, Image Processing, Computer Vision',
         socials: {
             linkedin: 'https://www.linkedin.com/in/ebenezer-owusu-883b3634/',
             twitter: '',
@@ -44,32 +49,35 @@ export const faculty = [
         }  
     },
     {
+        id:'MS',
         name: 'Michael Agbo Tettey Soli',
         title: 'Lecturer',
         img: 'https://www.polymorphlabs.io/static/media/Soli.1ce44e8e40e1a2140c76.webp',
-        description: 'From LinkedIn',
+        description: 'Senior Software Engineer | PhD Candidate | Co-Founder',
         socials: {
-            linkedin: '',
-            twitter: '',
-            facebook: ''
+            linkedin: 'https://gh.linkedin.com/in/codeoverdoze',
+            twitter: 'https://twitter.com/codeoverdoze',
+            facebook: 'https://www.facebook.com/codeoverdoze/'
         }  
     },
     {
+        id:'MAM',
         name: 'Mark Atta Mensah',
         title: 'Lecturer',
         img: 'https://yt3.googleusercontent.com/ayErlUnVQjMrqvyQ6l-T3Y3rl45WgVjZVLECbUP5iH44KhE1T-pVa20STjd5El6qmwhjIXEHFWY=s176-c-k-c0x00ffffff-no-rj',
-        description: 'From LinkedIn',
+        description: 'Mark Atta Mensah is a computer science instructor at the University of Ghana',
         socials: {
             linkedin: 'https://www.linkedin.com/in/markmensah/?originalSubdomain=gh',
-            twitter: '',
+            twitter: 'https://twitter.com/markkmensah',
             facebook: ''
         }  
     },
     {
+        id:'PA',
         name: 'Paul Nii Tackie Ammah',
         title: 'Lecturer',
         img: 'https://www.polymorphlabs.io/static/media/paul.830a8db989ed5598c07d.jpg',
-        description: 'From LinkedIn',
+        description: 'Computer Science lecturer in the University of Ghana',
         socials: {
             linkedin: '',
             twitter: '',
@@ -77,25 +85,96 @@ export const faculty = [
         }  
     },
     {
+        id:'CA',
         name: 'Christian Abrokwa',
         title: 'Teaching Assistant',
         img: '/img/ca.jpeg',
-        description: 'From Linkedin',
+        description: 'Software Engineering fellow ',
         socials: {
             linkedin: 'https://www.linkedin.com/in/daquiver/?originalSubdomain=gh',
-            twitter: '',
-            facebook: ''
+            twitter: 'https://twitter.com/Daquiver1',
+            facebook: 'https://web.facebook.com/christian.abrokwa/?_rdc=1&_rdr'
         }  
     },
     {
+        id:'PB',
         name: 'Phillipa Bennett-Eghan',
         title: 'Teaching Assistant',
         img: '/img/pbe.jpeg',
-        description: 'From LinkedIn',
+        description: 'Junior Software Engineer',
         socials: {
             linkedin: 'https://www.linkedin.com/in/phillipa-bennett-eghan/?originalSubdomain=gh',
             twitter: '',
             facebook: ''
         }  
     },
+]
+
+export const events = [
+    {
+        name: 'Tap Into Tech Ghana',
+        logo: 'https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F634535609%2F496415951365%2F1%2Foriginal.20231103-154610?w=940&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C61%2C1080%2C540&s=ef8466abc9755f0905f89ccc806a46b4',
+        organization: 'Black Girls in Tech',
+        date: '8 Dec, 2023',
+        status: 'upcoming'
+    },
+    {
+        name: 'Ghana Tech Summit 2023',
+        logo: 'https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F45405069%2F223517982631%2F2%2Foriginal.jpg?w=225&auto=format%2Ccompress&q=75&sharp=10&s=3bc7453d953793431789565c944438c3',
+        organization: 'Ghana Tech Summit',
+        date: '14 Dec, 2023',
+        status: 'upcoming'
+    },
+    {
+        name: 'Tech Connect Accra 2023',
+        logo: 'https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F377225669%2F1218537327013%2F1%2Foriginal.20221020-132930?w=512&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C0%2C2048%2C2048&s=e30403578526fcdc09bbcc96611711ba',
+        organization: 'Tech Connect Accra',
+        date: '27 Dec, 2023',
+        status: 'upcoming'
+    },
+    {
+        name: 'Dev Fest Accra 2023',
+        logo: 'https://pbs.twimg.com/profile_images/1599138219966218246/-PAbMD0E_400x400.jpg',
+        organization: 'Google Developer Groups -  Accra',
+        date: '18 Nov, 2023',
+        status: 'past'
+    },
+    {
+        name: 'TEDxAccra 2022',
+        logo: 'https://pi.tedcdn.com/r/www.filepicker.io/api/file/jBB7BQBXTZaoQebrGjnG?quality=90&w=260',
+        organization: 'TED',
+        date: '17 Dec, 2022',
+        status: 'past'
+    },
+
+]
+
+export const projects = [
+    {
+        id: "UL",
+        name: 'UnLost',
+        img: "/img/unlost.jpeg",
+        type: "Mobile Application",
+        author: "Christian Abrokwa",
+        link: "https://www.linkedin.com/company/unlostgh/",
+        description: "Introducing UnLost, the ultimate solution for students at the University of Ghana to find their missing ID cards! UnLost is designed to address the common problem of lost items, making it easier than ever to locate and recover missing ID cards. With a centralized platform for all missing items, UnLost streamlines the process of finding and returning ID cards to their rightful owners."
+    },
+    {
+        id:"NM",
+        name: 'Night Market',
+        img: "/img/night_market.jpg",
+        type: "Mobile Application",
+        author: "Abonten Technologies",
+        link: "https://www.nightmarketgh.com/",
+        description: "Night Market is a campus food delivery app that connects students with vendors and makes food accessible. It is currently available for University of Ghana, anywhere on campus. Buy from the Night Market and have it delivered to you in minutes"
+    },
+    {
+        id: "LU",
+        name: 'LocateU',
+        img: "/img/locateU.png",
+        type: "Web Application",
+        author: "Kwabena Owusu-Darko",
+        link: "https://www.linkedin.com/company/unlostgh/",
+        description: "Never lose your way on campus. LocateU allows educational institutions to provide accurate location data on all lecture halls, examination venues and administrative buildings to resident students."
+    }
 ]
