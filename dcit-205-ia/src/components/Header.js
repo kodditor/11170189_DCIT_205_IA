1b8acc0fd6d5a83a01077e710daac8c6b065185c
Kwabena Owusu-Darko <kobbyowusudarko@gmail.com>
2023-11-24 21:11:57 +0000

    Sticky Header

--- dcit-205-ia/src/components/Header.js
+++ dcit-205-ia/src/components/Header.js
@@ -1,21 +1,23 @@
-import '../styles/header.css'
+import styles from '../styles/header.module.css'
 import {Link} from 'react-router-dom'
+
 
 export default function Header(){
     return (
         <>
-            <nav id='main-nav'>
+            <nav id={styles.main_nav}>
                 <div>
-                    <img className='logo' alt='UG Department of Computer Science Logo' src='/img/logo.png'/>
+                <Link to='/' ><img className={styles.logo} alt='UG Department of Computer Science Logo' src='/img/logo.png'/></Link>
                 </div>
                 <div>
-                    <ul className='link-list'>
-                        <li>HOME</li>
-                        <li>PROGRAMS</li>
-                        <li>STUDENT PROJECTS</li>
-                        <li>FACULTY</li>
-                        <li>ABOUT US</li>
-                        <li><button>CONTACT US</button></li>
+                    <ul className={styles.link_list}>
+                        <Link to='/' ><li>HOME</li></Link>
+                        <Link to='/programs' ><li>PROGRAMS</li></Link>
+                        <Link to='/projects' ><li>STUDENT PROJECTS</li></Link>
+                        <Link to='/faculty' ><li>FACULTY</li></Link>
+                        <Link to='/events' ><li>EVENTS</li></Link>
+                        <Link to='/about' ><li>ABOUT US</li></Link>
+                        <Link to='/contact' ><li><button>CONTACT US</button></li></Link>
                     </ul>
                 </div>
             </nav>
