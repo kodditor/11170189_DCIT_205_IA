939c456fe6449141f153d860e4b0883e2d378100
Kwabena Owusu-Darko <kobbyowusudarko@gmail.com>
2023-11-24 21:16:30 +0000

    Funny bug with styling...

--- dcit-205-ia/src/components/discover.js
+++ dcit-205-ia/src/components/discover.js
@@ -1,9 +1,9 @@
-
+import styles from '../App.module.css';
 
 export default function Discovery(){
     return (
         <>
-            <section className="discovery">
+            <section className={styles.discovery}>
                 <h2>DISCOVER YOUR FUTURE IN COMPUTER SCIENCE</h2>
                 <p>Explore our cutting-edge programs and join Ghana's future tech force.</p>
                 <button className="secondary-button" style={{fontSize: '1.3rem', margin:'auto'}}>Apply Now</button>
